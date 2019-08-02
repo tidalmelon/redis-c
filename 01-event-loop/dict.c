3ddecad70f924bf1e8e72bfeb21007220e250d3e
tidalmelon <tidal.melon@gmail.com>
2019-08-02 19:08:44 +0800

    add

--- 01-event-loop/dict.c
+++ 01-event-loop/dict.c
@@ -8,7 +8,7 @@
 #include <ctype.h>
 #include <assert.h>
 #include "dict.h"
-//#include "zmalloc.h"
+#include "zmalloc.h"
 
 /*
  * 通过 dictEnableResize() 和 dictDisableResize() 两个函数.
@@ -69,10 +69,94 @@ uint32_t dictGetHashFunctionSeed(void) {
      return dict_hash_function_seed;
 }
 
+/* MurmurHash2, by Austin Appleby
+ * Note - This code makes a few assumptions about how your machine behaves -
+ * 1. We can read a 4-byte value from any address without crashing
+ * 2. sizeof(int) == 4
+ *
+ * And it has a few limitations -
+ *
+ * 1. It will not work incrementally.
+ * 2. It will not produce the same results on little-endian and big-endian
+ *    machines.
+ */
+unsigned int dictGenHashFunction(const void *key, int len) {
+	/* 'm' and 'r' are mixing constants generated offline.
+	They're not really 'magic', they just happen to work well.  */
+	uint32_t seed = dict_hash_function_seed;
+	const uint32_t m = 0x5bd1e995;
+	const int r = 24;
 
+	/* Initialize the hash to a 'random' value */
+	uint32_t h = seed ^ len;
 
+	/* Mix 4 bytes at a time into the hash */
+	const unsigned char *data = (const unsigned char *)key;
 
+	while (len >= 4) {
+		uint32_t k = *(uint32_t*)data;
 
+		k *= m;
+		k ^= k >> r;
+		k *= m;
+
+		h *= m;
+		h ^= k;
+
+		data += 4;
+		len -= 4;
+	}
+
+	/* Handle the last few bytes of the input array  */
+	switch (len) {
+	case 3: h ^= data[2] << 16;
+	case 2: h ^= data[1] << 8;
+	case 1: h ^= data[0]; h *= m;
+	};
+
+	/* Do a few final mixes of the hash to ensure the last few
+	* bytes are well-incorporated. */
+	h ^= h >> 13;
+	h *= m;
+	h ^= h >> 15;
+
+	return (unsigned int)h;
+}
+
+/* And a case insensitive hash function (based on djb hash) */
+unsigned int dictGenCaseHashFunction(const unsigned char *buf, int len) {
+	unsigned int hash = (unsigned int)dict_hash_function_seed;
+
+	while (len--)
+		hash = ((hash << 5) + hash) + (tolower(*buf++)); /* hash * 33 + c */
+	return hash;
+}
+
+/* ----------------------------- API implementation ------------------------- */
+
+/* 
+ * 重置 (或初始化) 给定哈希表的各项属性值
+ *
+ * T = o(1)
+ */
+static void _dictReset(dictht *ht) {
+     ht->table = NULL;
+     ht->size = 0;
+     ht->sizemask = 0;
+     ht->used = 0;
+ }
+
+/*
+ * 创建一个新的字典
+ *
+ * T = o(1)
+ */
+dict *dictCreate(dictType *type, 
+    void *privDataPtr) {
+    dict *d = zmalloc(sizeof(*d));
+    //pause
+                     
+}
 
 
 
